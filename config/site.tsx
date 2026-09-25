3778f81a656fec970066488e035b0c4dae625d1c
Arash Kazerouni <94610980+arashKazerouni@users.noreply.github.com>
2026-09-25 19:35:43 +0330

    build: initialize LEXO website (config/site.tsx)

---
+++ config/site.tsx
@@ -0,0 +1,7 @@
+export const site = {
+  name: "L for Lexora",
+  asset: "LEXO",
+  organization: "LEXORA",
+  supply: "100,000,000",
+  issuer: "GDSCUECSJ2UHOZJHSPXJEEXXQRRS5XQD62Y6XIGMFKQY5PBXNIJGXXRP",
+};
